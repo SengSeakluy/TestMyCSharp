ffdfa84d695acf2f66f5e2271cf308e05cbd2d37
SengSeakluy <seakluyseng456@gmail.com>
2024-11-06 15:00:11 +0700

    test

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+     static void Main()
+     {
+          Console.WriteLine("Enter the notification title:");
+          string? input = Console.ReadLine();
+          if (string.IsNullOrEmpty(input))
+          {
+               Console.WriteLine("The input is empty.");
+          }else{
+               string result = ParseNotificationChannels(input);
+               Console.WriteLine(result);
+          }
+     }
+
+     static string ParseNotificationChannels(string input)
+     {
+          // List of valid channels
+          var validChannels = new HashSet<string> { "BE", "FE", "QA", "Urgent" };
+          var matches = Regex.Matches(input, @"\[(.*?)\]");
+          var channels = new List<string>();
+
+          foreach (Match match in matches)
+          {
+               string tag = match.Groups[1].Value;
+               if (validChannels.Contains(tag))
+               {
+                    channels.Add(tag);
+               }
+          }
+
+          if (channels.Count > 0)
+          {
+               return "Receive channels: " + string.Join(", ", channels);
+          }else{
+               return "No channels found.";
+          }
+     }
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # TestMyCSharp
+
+#1. Install C#
+     + https://dotnet.microsoft.com/en-us/download
+     + version of the .NET SDK you installed
+          - dotnet --version
+     + Create a new .NET Core console application by running:
+          - dotnet new console -n TestMyCSharp
+     + Build the application by running: 
+          (This will compile your application and check for any errors.)
+          - dotnet build
+     + Run the application with the following command:
+          - dotnet run
+
+
+
